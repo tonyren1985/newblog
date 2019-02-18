450dfcb70ba023a8b8baca3abd2909ad8abd0ac3
tonyren1985 <275209395@qq.com>
2019-02-18 16:52:12 +0800

    增加文章修改功能，还未完成（使用makedown库完成blog编辑功能)

--- djangoblog/article/views.py
+++ djangoblog/article/views.py
@@ -5,6 +5,8 @@ from .forms import ArticleColumnForm, ArticlePostForm
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 @login_required(login_url='../../account/login/')
@@ -75,3 +77,53 @@ def article_post(request):
         article_columns = ArticleColumn.objects.filter(user=request.user)
         return render(request, 'article/column/article_post.html', {'article_post_form': article_post_form,
                                                                     'article_columns': article_columns })
+
+
+@login_required(login_url='../../account/login/')
+def article_list(request):
+    articles = ArticlePost.objects.filter(author=request.user)
+    return render(request, 'article/column/article_list.html', {'articles': articles})
+
+
+@login_required(login_url='../../account/login/')
+def article_detail(request, id, slug):
+    article = get_object_or_404(ArticlePost, id=id, slug=slug)
+    return render(request, 'article/column/article_detail.html', {'article': article})
+
+
+@login_required(login_url='../../account/login/')
+@require_POST
+@csrf_exempt
+def del_article(request):
+    article_id = request.POST['article_id']
+    try:
+        article = ArticlePost.objects.get(id=article_id)
+        article.delete()
+        return HttpResponse("1")
+    except:
+        return HttpResponse("2")
+
+
+@login_required(login_url='../../account/login/')
+@csrf_exempt
+def redit_article(request, article_id):
+    if request.method == "GET":
+        article_columns = request.user.article_column.all()
+        article = ArticlePost.objects.get(id=article_id)
+        this_article_form = ArticleColumnForm(initial={"title": article.title})
+        this_article_column = article.column
+        return render(request, "article/column/redit_article.html", {"article": article,
+                                                                     "article_columns": article_columns,
+                                                                     "this_article_column": this_article_column,
+                                                                     "this_article_form": this_article_form})
+    else:
+        redit_article = ArticlePost.objects.get(id=article_id)
+        try:
+            redit_article.column = request.user.article_column.get(id=request.POST['column_id'])
+            redit_article.title = request.POST['title']
+            redit_article.body = request.POST['body']
+            redit_article.save()
+            return HttpResponse("1")
+        except:
+            return  HttpResponse("2")
+
